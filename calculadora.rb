57d4e5089a2273825e9afb3303cd86952ba605bf
Diego Cirilo <eletronzip@gmail.com>
2013-02-23 03:35:03 -0300

    iniciando transição p/ OO

--- calculadora.rb
+++ calculadora.rb
@@ -1,32 +1,66 @@
+class RpnCalc
+  def initialize
+    @pilha = []
+  end
+
+  def pilha
+    return @pilha
+  end
+
+  def push number
+    @pilha.push number
+  end
+
+  def sum
+    @pilha.push @pilha.pop + @pilha.pop
+  end
+
+  def sub
+    temp = @pilha.pop
+    @pilha.push @pilha.pop - temp 
+  end
+
+  def mul
+    @pilha.push @pilha.pop * @pilha.pop
+  end
+
+  def div
+    temp = @pilha.pop
+    @pilha.push @pilha.pop / temp
+  end
+
+end
+
 def float?(num)
   return Float(num)
 rescue => e
   return false
 end
 
-a=[]
+calc = RpnCalc.new
+
 while true do
   puts "\e[H\e[2J"
   puts "======Pilha======"
-  puts a[-3]
-  puts a[-2]
-  puts a[-1]
+  puts calc.pilha[-3]
+  puts calc.pilha[-2]
+  puts calc.pilha[-1]
   puts "================="
 
   temp=gets.chomp
   break if temp=="sair"
   if float?(temp) then
-    a.push Float(temp)
+    calc.push Float(temp)
   else
     case
     when temp=="+"
-      a.push a.pop+a.pop
+      calc.sum
     when temp=="-"
-      a.push a.pop-a.pop
+      calc.sub
     when temp=="*"
-      a.push a.pop*a.pop
+      calc.mul
     when temp=="/"
-      a.push a.pop/a.pop
+      calc.div
     else
       puts "Operador Inválido!"
     end
